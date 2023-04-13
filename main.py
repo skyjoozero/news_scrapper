2c0172f4641e557cee08d07942c634e71efeb1f2
skyjoozero <skyjoozero@naver.com>
2023-04-14 01:02:48 +0900

    finish add function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+import json
+from collections import OrderedDict
 import pprint
 from time import sleep
 from bs4 import BeautifulSoup as bs
 
-date = '20230412'
+date = '20230414'
 page = 1
 
 main_url = 'https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2=258&sid1=101&date=' + date +'&page='
@@ -14,20 +16,41 @@ headers = {
 # contents = soup.select_one('td.content').find_all('a')
 # page_num = soup.select_one('div.paging').find('strong').getText()
 
-while True:
-    url = main_url + str(page)
+def get_soup(url_string):
+    main_page = requests.get(url_string, headers=headers)
+    return bs(main_page.content, "html.parser")
 
-    main_page = requests.get(url, headers=headers)
-    soup = bs(main_page.content, "html.parser")
+def getNewsTitles(contents):
+    arr = []
 
-    # contents = soup.select_one('td.content').find_all('a')
-    page_num = soup.select_one('div.paging').find('strong').getText()
+    for el in contents.find_all('a'):
+        arr.append(el.get_text().strip())
 
-    if str(page) != page_num:
-        break
-    else:
-        print(page)
-        page = page + 1
-        sleep(0.1)
+    return arr
+def getNewsUrls(contents):
+    arr = []
 
-# pprint.pprint(page_num)
+    for content in contents:
+        tempSoup = bs(str(content), "html.parser")
+        arr.append(tempSoup.find('a')['href'])
+
+    return arr
+
+if __name__ == '__main__':
+
+    while True:
+        soup = get_soup(main_url + str(page))
+        page_num = soup.select_one('div.paging').find('strong').getText()
+
+        contents = soup.select_one('div.list_body')
+        titles = getNewsTitles(contents)
+
+        contents = soup.find_all('dt', 'photo')
+        urls = getNewsUrls(contents)
+
+        if str(page) != page_num:
+            break
+        else:
+            print(page)
+            page = page + 1
+            sleep(0.1)
