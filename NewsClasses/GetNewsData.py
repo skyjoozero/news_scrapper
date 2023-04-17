1b2085ca66f4420ea239509562cfc1c36a89e83d
skyjoozero <skyjoozero@naver.com>
2023-04-17 23:36:17 +0900

    change user-agent

--- NewsClasses/GetNewsData.py
+++ NewsClasses/GetNewsData.py
@@ -11,7 +11,7 @@ from datetime import datetime
 class GetNewsData():
     fileIo = FileIo()
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Whale/3.19.166.16 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48'
     }
 
     def _getSoup(self, urlString):
